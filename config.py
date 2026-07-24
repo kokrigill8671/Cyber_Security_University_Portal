739e0cbbbaf11c05e5611f3d71ac1b9da6190ccd
Gurpreet Singh <kokrigill8671@gmail.com>
2026-07-24 21:39:19 +0530

    Fix Render deployment

--- config.py
+++ config.py
@@ -15,7 +15,9 @@ class Config:
 
     SECRET_KEY = "CyberUniversityPortal2026"
 
-    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:password@localhost/cyber_portal"
+    BASE_DIR = os.path(os.path.dirname(__file__))
+
+SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(BASE_DIR, "cyber_portal.db")
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
