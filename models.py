011a042beafa59c88a9dfd5ca32fb7d96cf127a8
Gurpreet Singh <kokrigill8671@gmail.com>
2026-07-26 01:15:15 +0530

    Remove registration expiry feature

--- models.py
+++ models.py
@@ -3,11 +3,13 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class Student(db.Model):
+    __tablename__ = "student"
+
     id = db.Column(db.Integer, primary_key=True)
 
-    first_name = db.Column(db.String(100))
-    last_name = db.Column(db.String(100))
-    email = db.Column(db.String(120), unique=True)
+    first_name = db.Column(db.String(100), nullable=False)
+    last_name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
     phone = db.Column(db.String(20))
     student_id = db.Column(db.String(50))
     course = db.Column(db.String(100))
@@ -15,10 +17,8 @@ class Student(db.Model):
     dob = db.Column(db.String(50))
     address = db.Column(db.Text)
 
-    username = db.Column(db.String(100), unique=True)
-    password = db.Column(db.String(200))
-
-    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    username = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
 
     def set_password(self, password):
         self.password = password
