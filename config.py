7f537b96929173708e6eadc8eabdcaef77b82fad
Gurpreet Singh <kokrigill8671@gmail.com>
2026-07-25 16:23:35 +0530

    Configure database URL

--- config.py
+++ config.py
@@ -15,3 +15,17 @@ class Config:
     SECRET_KEY = "cyberportal_secret_key"
     SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(BASE_DIR, "portal.db")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    
+    import os
+
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+
+class Config:
+    SECRET_KEY = "cyberportal_secret_key"
+
+    SQLALCHEMY_DATABASE_URI = os.getenv(
+        "DATABASE_URL",
+        "sqlite:///" + os.path.join(BASE_DIR, "portal.db")
+    )
+
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
